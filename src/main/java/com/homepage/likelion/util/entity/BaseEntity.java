ff26320853956644ac0c5d58de13b49621109472
khyaejin <olivia019182@naver.com>
2024-05-09 22:14:07 +0900

    ✨ BaseEntity 작성과 적용

---
+++ src/main/java/com/homepage/likelion/util/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.homepage.likelion.util.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) //JPA에서 Entity 생명주기 이벤트를 처리하기 위한 어노테이션
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
+}
