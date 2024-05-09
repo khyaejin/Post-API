d0df9fb246c053f9b2e72cb634a4651b8f457d5e
khyaejin <olivia019182@naver.com>
2024-05-09 20:14:46 +0900

    ✨ CustomApiRespons 클래스 작성

---
+++ src/main/java/com/homepage/likelion/util/response/CustomApiResponse.java
@@ -0,0 +1,25 @@
+package com.homepage.likelion.util.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+public class CustomApiResponse<T> {
+
+    //status, data, message
+    private int status;
+    private T data;
+    private String message;
+
+    //성공
+    public static <T> CustomApiResponse<T> createSucsess(int status, T data, String message) {
+        return new CustomApiResponse<>(status, data, message);
+    }
+    //실패
+    public static <T> CustomApiResponse<T> createFailWithout(int status, String message) {
+        return new CustomApiResponse<>(status, null, message);
+    }
+
+}
