8b0f0d3091c0a7047f2568f8a4346bdcdcdfdedd
khyaejin <olivia019182@naver.com>
2024-05-09 19:01:03 +0900

    📝 Feat : TestController - @GetMapping 사용

---
+++ src/main/java/com/homepage/likelion/test/SimpleTestController.java
@@ -0,0 +1,23 @@
+package com.homepage.likelion.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //SimpleTestController가 RestPoolwebService의 컨트롤러임을 알려주는 어노테이션?
+@RequestMapping("/api/simpleText") //원래 @GetMapping("/api/simpleText/success")이런식으로 써야하는데 "/api/simpleText/fail"와 /api/simpleText이부분이 겹치니까 미리 정해둬서 편하게 쓰는 것
+public class SimpleTestController {
+
+    // GET http://localhost:8080/api/simpleText/success
+    @GetMapping("success")
+    public String simpleTextSuccess(){
+        return "안녕";
+    }
+
+    // GET http://localhost:8080/api/simpleText/fail
+    @GetMapping("fail")
+    ///@RequestMapping("/api/simpleText") 없으면 api/simpleText/fail 이렇게 써야 함
+    public String simpleTextFail(){
+        return "안녕";
+    }
+}
